6153728d8cdcd380bee138cf21195d64f35082d8
Oscarsalazar03 <oscar.salazar@est.iudigital.edu.co>
2025-03-28 16:26:30 +0000

    actividad-3

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="actividad_2",
+    name="actividad_2 y actividad_3",
     version="0.0.1",
     authors="Óscar Alexander Salazar Alvarez y Sebastian Echeverri Garces",
     author_email="",
     description="",
-    py_modules=["actividad_2"],
+    py_modules=["actividad_2", "actividad_3"],
     install_requires=[
+        "matplotlib>=3.5.0",
+        "numpy",
         "pandas",
         "openpyxl",
         "requests"
